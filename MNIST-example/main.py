eaef5e626d2f9e137e68f44437d3d7b50a0104ea
rbonazzola <rodbonazzola@gmail.com>
2023-05-31 12:45:51 +0100

    Fixes to MNIST main.py script

--- MNIST-example/main.py
+++ MNIST-example/main.py
@@ -1,14 +1,55 @@
 import pytorch_lightning as ptl
 import mlflow
-from MNIST_example.MNIST import MNISTClassifier
 from MNIST_lightning import CNN_Module, MNIST_DataModule 
 from my_ptl_callbacks import early_stopping, model_checkpoint, progress_bar, rich_model_summary
 
+import torch
+from torch import nn
+import torch.nn.functional as F
 
-if __name__ == "__main__:
+class MNISTClassifier(nn.Module):
 
-    mlflow.set_tracking_uri(uri)
+    def __init__(self):
+        super(MNISTClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.softmax(x, dim=1)
+        return output
+
+
+if __name__ == "__main__":
+
+
+    import argparse
     
+    parser = argparse.ArgumentParser("MNIST training")
+    parser.add_argument("--batch_size", default=512)
+    parser.add_argument("--precision", default="16")
+    parser.add_argument("--mlflow_exp_name", default="test")
+    
+    args = parser.parse_args()
+    
+    uri = "./mlruns"
+    mlflow.set_tracking_uri(uri)
+   
+    exp_name = args.mlflow_exp_name
     try:
         exp_id = mlflow.create_experiment(exp_name)
     except:
@@ -19,15 +60,7 @@ if __name__ == "__main__:
     
     callbacks = [ early_stopping, model_checkpoint, progress_bar, rich_model_summary ]
 
-    import argparse
-    
-    parser = argparse.ArgumentParser("MNIST training")
-    parser.add_argument("--batch_size", default=512)
-    parser.add_argument("--precision", default="16")
-    
-    args = parser.parse_args()
-
-    mlflow.log_parameters({"precision": args.precision})
+    mlflow.log_params({"precision": args.precision})
     
     model = MNISTClassifier()
     datamodule = MNIST_DataModule(batch_size=args.batch_size, split_lengths=[48000, 12000])
